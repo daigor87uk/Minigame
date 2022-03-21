74a4d9be011b6dfa42b7127047e97b861bf01a5a
daigor <daigor.rodrigues@gmail.com>
2022-03-21 16:24:10 +0000

    DR:final commit

--- Minigame.java
+++ Minigame.java
@@ -1,81 +1,84 @@
+// Java program for the above approach
 import java.util.Scanner;
- 
+
 public class Minigame {
- 
-    // Function that implements the
-    // number guessing game
-    public static void
-    guessingNumberGame()
-    {
-        try (// Scanner Class
-		Scanner sc = new Scanner(System.in)) {
-			// Generate the numbers
-			int number = 1 + (int)(100
-			                       * Math.random());
- 
-			// Given K tries
-			int K = 5;
- 
-			int i, guess;
- 
+
+	// Function that implements the
+	// number guessing game
+	public static void
+	guessingNumberGame()
+	{
+		// Scanner Class
+		Scanner sc = new Scanner(System.in);
+
+		// Generate the numbers
+		int number = 1 + (int)(100
+							* Math.random());
+
+		// Given K Tries
+		int K = 5;
+
+		int i, guess;
+
+		System.out.println(
+			"A number is chosen"
+			+ " between 1 to 100."
+			+ "Guess the number"
+			+ " within 5 Tries.");
+
+		// Iterate over K Tries
+		for (i = 0; i < K; i++) {
+
 			System.out.println(
-			    "A number is chosen"
-			    + " between 1 to 100."
-			    + "Guess the number "
-			    + " within 5 tries.");
- 
-			// Iterate over K Tries
-			for (i = 0; i < K; i++) {
- 
-			    System.out.println(
-			        "Guess the number:");
- 
-			    // Take input for guessing
-			    guess = sc.nextInt();
- 
-			    // If the number is guessed
-			    if (number == guess) {
-			        System.out.println(
-			            "Congratulations!"
-			            + " You guessed the number.");
-			        break;
-			    }
-			    else if (number > guess
-			             && i != K - 1) {
-			        System.out.println(
-			            "The number is "
-			            + "greater than " + guess);
-			    }
-			    else if (number < guess
-			             && i != K - 1) {
-			        System.out.println(
-			            "The number is"
-			            + " less than " + guess);
-			    }
+				"Guess the number:");
+
+			// Take input for guessing
+			guess = sc.nextInt();
+
+			// If the number is guessed
+			if (number == guess) {
+				System.out.println(
+					"Congratulations!"
+					+ " You guessed the number.");
+				break;
 			}
- 
-			if (i == K) {
-			    System.out.println(
-			    		"You have exhausted "
-			    				+ K + " tries.");
- 
-			    System.out.println(
-			        "The number was " + number);
+			else if (number > guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is "
+					+ "greater than " + guess);
+			}
+			else if (number < guess
+					&& i != K - 1) {
+				System.out.println(
+					"The number is"
+					+ " less than " + guess);
 			}
 		}
-    }
- 
-    // Driver Code
-    public static void
-    main(String arg[])
-    {
- 
-        // Function Call
-        guessingNumberGame();
-    }
+
+		if (i == K) {
+			System.out.println(
+				"You have exhausted"
+				+ " K Tries.");
+
+			System.out.println(
+				"The number was " + number);
+		}
+	}
+
+	// Driver Code
+	public static void
+	main(String arg[])
+	{
+
+		// Function Call
+		guessingNumberGame();
+	}
 }
 
+//Contributed and modified by:
 //Daigor Rodrigues
 //Allister Gardner
 //Paul McFetridge
 //James Almond
+//Moheen Akhtar
